2147f81ba8cae2c4fc0418e9b55ac83d6ce3bf66
Hima79 <121005239+Himanshu7901@users.noreply.github.com>
2023-09-30 17:53:15 +0530

    Update joins2.sql

--- joins2.sql
+++ joins2.sql
@@ -16,3 +16,11 @@ FROM
     Weather AS W ON F.origin = W.origin
 WHERE
     F.dep_delay > 60 AND F.origin = 'JFK';
+
+ // As we want to take data only from left table and not from right table then we can use 
+Select *
+from Table A
+Left Join Table B
+     on A.id = B.id
+    
+Where B.id Is Null;
